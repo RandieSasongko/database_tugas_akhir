4098fec28ffafb4ac1d6e69cb2e5aadf24833a14
RandieSasongko <113977205+RandieSasongko@users.noreply.github.com>
2025-03-13 17:35:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -74,14 +74,14 @@ type Claims struct {
 func initDB() {
 	var err error
 
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"), // Ini harus dari Railway
-		os.Getenv("DB_PORT"), // Biasanya 3306
-		os.Getenv("DB_NAME"),
-	)
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	host := "mysql.railway.internal"
+	port := "3306"
+	database := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, database)
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
