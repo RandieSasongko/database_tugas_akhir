613a0677430834136b760fc01547fffd81b4ee3d
RandieSasongko <113977205+RandieSasongko@users.noreply.github.com>
2025-03-13 16:56:55 +0700

    Update main.go

--- main.go
+++ main.go
@@ -64,8 +64,15 @@ type Claims struct {
 }
 
 func initDB() {
-	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/compere_tugasakhir?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
